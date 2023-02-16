41513b174479ce3fc4fb9dfe386a4e6875d81386
Tomas Scerbak <tomas.scerbak@gmail.com>
2023-02-16 11:28:59 +0100

    created theme context and theme provider

--- src/store/theme-context.jsx
+++ src/store/theme-context.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ThemeContext = React.createContext({
   themeOne: false,
   themeTwo: true,
   themeThree: false,
   inputRef: null,
+  toogleTheme: () => {},
 });
 
-export default ThemeContext;
+const ThemeProvider = ({ children }) => {
+  const [themeOne, setThemeOne] = useState(false);
+  const [themeTwo, setThemeTwo] = useState(true);
+  const [themeThree, setThemeThree] = useState(false);
+
+  const toggleTheme = () => {
+    if (inputRef === "0") {
+      setThemeOne(themeOne);
+    } else if (inputRef === "1") {
+      setThemeTwo(themeTwo);
+    } else if (inputRef === "2") {
+      setThemeThree(themeThree);
+    }
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{ themeOne, themeTwo, themeThree, toggleTheme }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default { ThemeContext, ThemeProvider };
